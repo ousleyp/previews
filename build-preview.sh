0c9621a1bec92635db0e0d37505f80969e8aba7b
Pan Ousley <pousley@redhat.com>
2023-06-09 17:37:39 -0400

    updating build script

--- build-preview.sh
+++ build-preview.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 #
 # prerequisites:
-# * you changed the username in the `open` command (line 18) to your own
+# 
 # * your openshift-docs and previews directories have the same parent
 # * you are in the openshift-docs directory when running this script
 # * you checked out the branch that you want to build
+#
+# usage:
+#
+# * for an openshift-enterprise build, run $ </path/to/script>/<file_name>.sh
+# * for an OKD build, run $ <path/to/script>/<file_name>.sh okd
 
 asciibinder
 
-BRANCH=$(git branch --show-current) 
+BRANCH=$(git branch --show-current)
+GITUSER=$(git config --get remote.origin.url | sed -r 's/git@github.com://; s/\/openshift-docs.git//')
 
-cp -r _preview/openshift-enterprise/$BRANCH ../previews
+if [ $1 = "okd" ]; then
+  DISTRO="openshift-origin"
+  DIR="previews/okd"
+else
+  DISTRO="openshift-enterprise"
+  DIR="previews"
+fi
+
+cp -r _preview/$DISTRO/$BRANCH ../$DIR
 cd ../previews
 git add .
 git commit -m "init"
 git push
-open https://ousleyp.github.io/previews/$BRANCH/welcome/
+
+cd -
+
+echo -e "\nCheck the status of your build: https://github.com/$GITUSER/previews/actions 
+\nView the preview: https://$GITUSER.github.io/$DIR/$BRANCH/welcome/"
+
+open https://github.com/$GITUSER/previews/actions
+#open https://$GITUSER.github.io/$DIR/$BRANCH/welcome/
 
 # next steps:
 # 1. navigate to the relevant page(s) and get the link(s)
