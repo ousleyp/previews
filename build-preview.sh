a61a7e2903cef9b33a160563996343df6e885d5d
Pan Ousley <pousley@redhat.com>
2023-06-09 18:08:25 -0400

    comments

--- build-preview.sh
+++ build-preview.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# prerequisites:
+# 
+# * your openshift-docs and previews directories have the same parent
+# * you are in the openshift-docs directory when running this script
+# * you checked out the branch that you want to build
+#
+# usage:
+#
+# * for an openshift-enterprise build, run $ </path/to/script>/<file_name>.sh
+# * for an OKD build, run $ <path/to/script>/<file_name>.sh okd
 
 BRANCH=$(git branch --show-current)
 GITUSER=$(git config --get remote.origin.url | sed -r 's/git@github.com://; s/\/openshift-docs.git//')
@@ -31,4 +41,9 @@ echo -e "\nCheck the status of your build: https://github.com/$GITUSER/previews/
 \nView the preview: https://$GITUSER.github.io/$DIR/$BRANCH/welcome/"
 
 open https://github.com/$GITUSER/previews/actions
-#open https://$GITUSER.github.io/$DIR/$BRANCH/welcome/
+#open https://$GITUSER.github.io/$DIR/$BRANCH/welcome/ #404s if build is not done
+
+# next steps:
+# 1. navigate to the relevant page(s) and get the link(s)
+# 2. post the direct link(s) to your PR for review
+# 3. regularly clean up old preview builds
